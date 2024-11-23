dccc6c9d0176bc0493953df857ea3b7dd3ab81ec
Vadim <a-yva-2013@mail.ru>
2024-11-23 04:13:22 +0400

    добавление хедера и футера

---
+++ src/shared/icons/Time.tsx
@@ -0,0 +1,27 @@
+const Time = () => {
+  return (
+    <svg
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="currentColor"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M11 3.75C14.8598 3.75 18 6.89019 18 10.75C18 14.6098 14.8598 17.75 11 17.75C7.14019 17.75 4 14.6098 4 10.75C4 6.89019 7.14019 3.75 11 3.75ZM11 2.25C6.30558 2.25 2.5 6.05558 2.5 10.75C2.5 15.4444 6.30558 19.25 11 19.25C15.6944 19.25 19.5 15.4444 19.5 10.75C19.5 6.05558 15.6944 2.25 11 2.25Z" />
+      <path
+        d="M11 6.5V11"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.9998 11L7.96875 12.75"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export { Time };
