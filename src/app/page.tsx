ac8c995afa905006d902881219e4e07e80ce93c8
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-24 17:56:38 +0300

    UI:Buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-full flex gap-5 p-20 bg-[#EBEFF3]">
-      <div className="flex flex-col bg-white items-center p-8 gap-5 min-w-fit shadow-lg">
-        <h1>Primary button</h1>
+    <div className="h-full flex flex-wrap gap-5 p-20 bg-[#EBEFF3]">
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
+      <h1 className="text-white py-5">Secondary button</h1>
         <Button variant="default" >
           Записаться онлайн 
         </Button>
@@ -14,8 +14,8 @@ export default function Home() {
         </Button>
       </div>
 
-      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 min-w-fit shadow-lg">
-        <h1 className="text-white">Secondary button</h1>
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
+        <h1 className="text-white py-5">Secondary button</h1>
         <Button variant="secondary">
           Все услуги <span className="ms-[5px] mt-[3px]"><ChevronRight width={17} /></span>
         </Button>
@@ -24,13 +24,33 @@ export default function Home() {
         </Button>
       </div>
 
-      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 min-w-fit shadow-lg">
-        <h1 className="text-white">Stroke button</h1>
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
+        <h1 className="text-white py-5">Stroke button</h1>
         <Button variant="stroke">
           Подробнее
         </Button>
         <Button variant="stroke"  disabled>
           Подробнее
+        </Button>
+      </div>
+
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
+        <h1 className="text-white py-5">Stroke button</h1>
+        <Button variant="white">
+          Записаться онлайн
+        </Button>
+        <Button variant="white"  disabled>
+          Записаться онлайн
+        </Button>
+      </div>
+
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
+        <h1 className="text-white py-5">Stroke button</h1>
+        <Button variant="tretiary">
+          Смотреть все 
+        </Button>
+        <Button variant="tretiary"  disabled>
+          Смотреть все 
         </Button>
       </div>
     </div>
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,14 +3,16 @@ import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/shared/lib/utils";
+import { ChevronRight } from "lucide-react";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-[8px] text-[13px] tracking-tight font-medium disabled:pointer-events-none ",
+  "inline-flex items-center justify-center whitespace-nowrap rounded-[8px] text-[14px] tracking-tight font-bold disabled:pointer-events-none ",
   {
     variants: {
       variant: {
         default: `
           h-[40px] w-[182px] 
+          text-[13px]
           bg-primary text-primary-foreground
           transition-[font-size,background-color]
           duration-500 ease-out
@@ -21,8 +23,8 @@ const buttonVariants = cva(
         secondary: `
           h-[48px] w-[260px] 
           bg-transparent
-          text-primary text-[15px] font-bold
-          border border-primary border-[1px] rounded-[8px] 
+          text-primary text-[15px]
+          border border-primary border-[1px]
           transition-all duration-300 ease-out
           hover:opacity-100 hover:bg-primary hover:text-secondary
           active:bg-primary active:opacity-100 active:text-[13px]
@@ -30,23 +32,33 @@ const buttonVariants = cva(
         `,
         stroke: `
           h-[36px] w-[105px]
-          text-[#353535] text-[12px]
+          text-[#353535] text-[12px] font-semibold
           bg-transparent border border-[#353535] rounded-full
           transition-all duration-300 ease-out
           hover:bg-primary hover:text-secondary
           active:delay-[100ms] active:duration-[300ms] active:text-[11px]
           disabled:border-[#C6C6C6] disabled:bg-transparent disabled:text-[#C6C6C6]
         `,
-        outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
-        link: "text-primary underline-offset-4 hover:underline",
+        white: `
+          h-[40px] w-[182px] 
+          text-[13px]
+          bg-[#FFFFFF]
+          transition-all duration-300 ease-out
+          hover:border-[1px] hover:border-[#0084FF] hover:bg-transparent hover:text-primary
+          active:border-[1px] active:border-[#0084FF] active:delay-[100ms] active:text-[12px]
+          disabled:bg-[#404040] disabled:text-[#595959]
+        `,
+        tretiary: `
+          transition-all duration-300 ease-out
+          text-[#1467E1] 
+          hover:text-[#0084FF] 
+          active:text-[#0084FF]
+          active:transition-[spring]  active:[transition-duration:300ms] active:[transition-timing-function:cubic-bezier(0.17, 0.67, 0.83, 0.67)]
+          disabled:text-[#C6C6C6]
+        `,
       },
       size: {
         default: "",
-        sm: "h-9 rounded-md px-3",
-        lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
       },
     },
     defaultVariants: {
@@ -54,8 +66,7 @@ const buttonVariants = cva(
       size: "default",
     },
   }
-)
-
+);
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
@@ -64,14 +75,27 @@ export interface ButtonProps
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, asChild = false, ...props }, ref) => {
+  ({ className, variant, children, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
       <Comp
         className={cn(buttonVariants({ variant, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {variant === "tretiary" ? (
+          <div className="relative flex items-center">
+            <span className="transition-transform duration-300 ease-out hover:-translate-x-1">
+              {children}
+            </span>
+            <span className="ml-[5px] mt-[3px]">
+              <ChevronRight width={17} />
+            </span>
+          </div>
+        ) : (
+          children
+        )}
+      </Comp>
     );
   }
 );
