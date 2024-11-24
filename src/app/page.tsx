6e8969a7c05a14d0dc4f6049403d28a5c2ff3f26
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-24 15:45:12 +0300

    UI: Secondary button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="h-full my-auto p-20 ">
-      <div className="flex flex-col items-center p-8 gap-5 w-60 shadow-lg">
+    <div className="h-full flex gap-5 p-20 bg-[#EBEFF3]">
+      <div className="flex flex-col bg-white items-center p-8 gap-5 min-w-fit shadow-lg">
         <h1>Primary button</h1>
         <Button variant="default" size="default">
-          Primary Button
+          Записаться онлайн 
         </Button>
         <Button variant="default" size="default" disabled>
-          Disabled Button
+          Записаться онлайн
+        </Button>
+      </div>
+      <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 min-w-fit shadow-lg">
+        <h1 className="text-white">Secondary button</h1>
+        <Button variant="secondary" size="default">
+          Все услуги <span className="ms-[5px] mt-[3px]"><ChevronRight width={17} /></span>
+        </Button>
+        <Button variant="secondary" size="default" disabled>
+          Все услуги <ChevronRight width={17} />
         </Button>
       </div>
     </div>
