81cf910fe5e6886adfa7fad6e061d3e2c6f1a054
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-26 20:34:48 +0300

    page: oklinike

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,60 +1,17 @@
 import { Button } from "@/components/ui/button";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { ChevronRight } from "lucide-react";
-import Image from "next/image";
-import { Separator } from "@/components/ui/separator";
 import { Badge } from "@/components/ui/badge";
-import ImageCard from "@/components/ui/image-card";
 import { CardsWrapper } from "@/components/card/cards-wrapper";
 import { Container } from "@/components/ui/container";
-
-const CardsMocData = [
-  {
-    id: "1",
-    title: "PRP-терапия 1",
-    description:
-      "1 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
-    price: "1000",
-  },
-  {
-    id: "2",
-    title: "PRP-терапия 2",
-    description:
-      "2 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
-    price: "2000",
-  },
-  {
-    id: "3",
-    title: "PRP-терапия 3",
-    description:
-      "3 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
-    price: "3000",
-  },
-  {
-    id: "4",
-    title: "PRP-терапия 4",
-    description:
-      "4 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
-    price: "4000",
-  },
-];
-
-const ImageCardMocData = {
-  id: 1,
-  imageUrl:
-    "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
-  altText: "string",
-  tagData: [
-    { id: 1, label: "PRP-терапия" },
-    { id: 2, label: "PRP-терапия" },
-    { id: 3, label: "PRP-терапия" },
-    { id: 3, label: "PRP-терапия" },
-    { id: 3, label: "PRP-терапия" },
-  ],
-};
-
-const BlueImageUrl =
-  "https://images.unsplash.com/photo-1593012370132-c4390ff79e92?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+import { AllButtons } from "@/components/all-buttons";
+import { SectionHeader } from "@/components/section-header";
+import {
+  BlueImageUrl,
+  CardsMocData,
+  ImageCardMocData,
+} from "@/shared/const/moc-data";
+import ImageCard from "@/components/ui/image-card";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -75,16 +32,13 @@ export default function Home() {
 
       <section className="pt-[64px] pb-[100px]">
         <div className="container mx-auto max-w-[1364px] px-[20px]">
-          <div className="flex justify-between mb-[48px] ">
-            <h2>Услуги клиники</h2>
-            <p className="text-[#595959] text-[16px] leading-[22px] md:max-w-[540px]">
-              Наши специалисты используют передовые методы и индивидуальный
+          <SectionHeader
+            className="mb-[40px]"
+            title="Услуги клиники"
+            description="Наши специалисты используют передовые методы и индивидуальный
               подход, чтобы вы вновь почувствовали уверенность в своих
-              движениях.
-            </p>
-          </div>
-
-          <Separator className="mb-[40px]" />
+              движениях."
+          />
 
           <div className="mb-[78px]">
             <div className="flex justify-between mb-[16px]">
@@ -127,78 +81,20 @@ export default function Home() {
       </section>
 
       <section className="bg-[#EBEFF3] pt-[36px] pb-[100px]">
-          <Container>
-            <AspectRatio ratio={1324 / 470}>
-              <Image
-                width={1324}
-                height={470}
-                src={BlueImageUrl}
-                alt={"altText"}
-                className="h-full w-full object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
-              />
-            </AspectRatio>
-          </Container>
+        <Container>
+          <AspectRatio ratio={1324 / 470}>
+            <Image
+              width={1324}
+              height={470}
+              src={BlueImageUrl}
+              alt={"altText"}
+              className="h-full w-full object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
+            />
+          </AspectRatio>
+        </Container>
       </section>
 
-      <div className="h-full flex flex-wrap gap-5 p-20 bg-[#EBEFF3]">
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">Secondary button</h1>
-          <Button variant="default">Записаться онлайн</Button>
-          <Button variant="default" disabled>
-            Записаться онлайн
-          </Button>
-        </div>
-
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">Secondary button</h1>
-          <Button variant="secondary">
-            Все услуги{" "}
-            <span className="ms-[5px] mt-[3px]">
-              <ChevronRight width={17} />
-            </span>
-          </Button>
-          <Button variant="secondary" disabled>
-            Все услуги{" "}
-            <span className="ms-[5px] mt-[3px]">
-              <ChevronRight width={17} />
-            </span>
-          </Button>
-        </div>
-
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">Stroke button</h1>
-          <Button variant="stroke">Подробнее</Button>
-          <Button variant="stroke" disabled>
-            Подробнее
-          </Button>
-        </div>
-
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">Stroke button</h1>
-          <Button variant="white">Записаться онлайн</Button>
-          <Button variant="white" disabled>
-            Записаться онлайн
-          </Button>
-        </div>
-
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">tretiary button</h1>
-          <Button variant="tretiary">Смотреть все</Button>
-          <Button variant="tretiary" disabled>
-            Смотреть все
-          </Button>
-        </div>
-
-        <div className="flex flex-col bg-[#1E1E1E] items-center p-8 gap-5 w-[350px] shadow-lg">
-          <h1 className="text-white py-5">arrow button</h1>
-          <Button variant="arrow">
-            <ChevronRight width={20} />
-          </Button>
-          <Button variant="arrow" disabled>
-            <ChevronRight width={20} />
-          </Button>
-        </div>
-      </div>
+      <AllButtons />
     </div>
   );
 }
