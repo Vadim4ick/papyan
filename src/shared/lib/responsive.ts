3a03ae00f38ed223d5c73c0ef6ea0cb715cdbbeb
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-30 00:29:45 +0300

    ua-parser for ismobile detection

---
+++ src/shared/lib/responsive.ts
@@ -0,0 +1,17 @@
+"use server" 
+
+import {headers} from  'next/headers' ; 
+import { UAParser } from  'ua-parser-js' ; 
+
+export  const  isMobileDevice = ( ) => { 
+  if ( typeof process === 'undefined' ) { 
+    throw  new  Error ( '[Server method] вы импортируете серверный модуль за пределами сервера' ); 
+  } 
+
+  const { get } = headers (); 
+  const ua = get ( 'user-agent' ); const 
+
+  device = new  UAParser (ua || '' ) .getDevice (); 
+
+  return device.type === 'mobile' ; 
+};
