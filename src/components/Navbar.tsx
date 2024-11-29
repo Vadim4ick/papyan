b651e79ef8ebb87bc650d431053071ab349eb67b
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-29 15:17:00 +0300

    header плюс стили

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,25 @@
 import { navbar } from "@/shared/const/const";
 import Link from "next/link";
+import { Separator } from "./ui/separator";
+import { ChevronRight } from "lucide-react";
 
 const Navbar = () => {
   return (
-    <nav className="flex flex-col items-start gap-6 text-sm font-medium lg:flex-row lg:items-center">
+    <nav className="flex flex-col items-start text-sm font-medium lg:flex-row lg:space-x-4">
       {/* <div className="flex items-center gap-4 bg-[#EBEFF3] rounded-[8px]"> */}
-        {navbar.map((item) => (
+      {navbar.map((item, index) => (
+        <div key={item.id} className="h-full w-full flex ">
           <Link
-            className="text-[#454545] font-semibold leading-[19px] text-[14px] tracking-[0.28px] py-[11px] px-[45px] relative before:w-[1px] before:h-[24px] last:before:content-none before:bg-[#DEE2E5] before:absolute before:right-[2px] before:top-1/2 before:-translate-y-1/2"
-            key={item.id}
+            className="text-[#454545] font-semibold leading-[19px] text-[14px] tracking-[0.28px] py-[11px] lg:px-[45px] w-full flex lg:w-max"
             href={item.path}
           >
-            {item.title}
+            {" "}
+            <span className="flex-1">{item.title}</span>
+            <ChevronRight width={20} className="lg:hidden"/>
           </Link>
-        ))}
+          {index < navbar.length - 1 && <Separator orientation="vertical" />}
+        </div>
+      ))}
       {/* </div> */}
     </nav>
   );
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,25 +3,38 @@ import { Container } from "../ui/container";
 import Link from "next/link";
 import { Navbar } from "../Navbar";
 import { Button } from "../ui/button";
-import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "../ui/sheet";
+import {
+  Sheet,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTrigger,
+} from "../ui/sheet";
 import { Menu } from "lucide-react";
 
 const Header = () => {
   return (
-    <header className="h-[64px] border border-b-[#E8E8E8] sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="h-[64px] border border-b-[#E8E8E8] sticky top-0 z-40 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <Container className="h-full flex items-center">
         <div className="mr-2 lg:hidden">
           <Sheet>
+
             <SheetTrigger asChild>
               <Button variant="arrow">
                 <Menu className="h-5 w-5" />
               </Button>
             </SheetTrigger>
-            <SheetContent side="left">
+
+            <SheetContent side="left" className="flex flex-col">
               <SheetHeader className=" mb-5 border-b pb-5">
                 <Logo />
               </SheetHeader>
+              <div className="flex-1">
               <Navbar />
+              </div>
+              <SheetFooter>
+                <Button variant="secondary" className="w-full" >Записаться онлайн</Button>
+              </SheetFooter>
             </SheetContent>
           </Sheet>
         </div>
@@ -30,19 +43,16 @@ const Header = () => {
           <Logo />
         </Link>
 
-        <div className="hidden lg:flex flex-1">
+        <div className="hidden lg:flex flex-1 justify-center">
           <Navbar />
         </div>
 
         <div className="hidden lg:flex">
           <Button variant="secondary">Записаться онлайн</Button>
         </div>
-
-
       </Container>
     </header>
   );
 };
 
 export { Header };
-
