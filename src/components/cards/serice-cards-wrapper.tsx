5e74e1af859546a20d3e682a9b8128a1f6919b67
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-30 16:51:05 +0300

    client routing link from servise card

--- src/components/cards/serice-cards-wrapper.tsx
+++ src/components/cards/serice-cards-wrapper.tsx
@@ -14,6 +14,7 @@ interface ServiceCardsWrapperProps {
   cardsList: IServiceCards[];
 }
 
+
 const ServiceCardsWrapper: React.FC<ServiceCardsWrapperProps> = ({ cardsList }) => {
   const isMobile = useClientMediaQuery('(max-width: 1280px)')
   const numberOfCardsToRender = isMobile ? 2 : 4;
