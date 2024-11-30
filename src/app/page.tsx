cf6d3587c6c3a667e4a54413fba2c7869e04111a
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-11-30 12:26:42 +0300

    category wrapper - responcive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,26 @@
-import { Button } from "@/components/ui/button";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
-import { Badge } from "@/components/ui/badge";
-import { ServiceCardsWrapper } from "@/components/cards/serice-cards-wrapper";
 import { Container } from "@/components/ui/container";
 import { SectionHeader } from "@/components/section-header";
 import {
   BlueImageUrl,
-  CardsMocData,
-  ImageCardMocData,
   ImageMocData,
 } from "@/shared/const/moc-data";
-import ImageCard from "@/components/ui/image-card";
 import Image from "next/image";
 import { SliderWrapper } from "@/components/slider/slider-wrapper";
+import { CatigoryWrapper } from "@/components/catigory/catigory-wrapper";
 
 export default function Home() {
   return (
     <div className="">
       <section className="h-dvh bg-[#EBEFF3] lg:px-[20px] lg:pt-[36px] lg:pb-[100px]">
         <div className="container h-full mx-auto max-w-[1364px] ">
-            <Image
-              width={1324}
-              height={727}
-              src={BlueImageUrl}
-              alt={"altText"}
-              className="h-full object-cover lg:rounded-lg transition-transform duration-300 group-hover:scale-105"
-            />
+          <Image
+            width={1324}
+            height={727}
+            src={BlueImageUrl}
+            alt={"altText"}
+            className="h-full object-cover lg:rounded-lg transition-transform duration-300 group-hover:scale-105"
+          />
         </div>
       </section>
 
@@ -38,44 +33,11 @@ export default function Home() {
               подход, чтобы вы вновь почувствовали уверенность в своих
               движениях."
           />
+          
+          {/* ToDo реализовать передачу обьекта Категории */}
+          <CatigoryWrapper className="mb-[78px]" />
 
-          <div className="mb-[78px]">
-            <div className="flex justify-between mb-[16px]">
-              <div className="flex gap-1">
-                <h3>Физиотерапия</h3>
-                <Badge variant="default">10</Badge>
-              </div>
-              <Button variant="tretiary" className="hidden md:block">Смотреть все</Button>
-            </div>
-
-            <div className="flex flex-wrap justify-between gap-[130px]">
-              <ImageCard
-                imageData={ImageCardMocData}
-                width={380}
-                height={418}
-              />
-              <ServiceCardsWrapper cardsList={CardsMocData} />
-            </div>
-          </div>
-
-          <div className="mb-[78px]">
-            <div className="flex justify-between mb-[16px]">
-              <div className="flex gap-1">
-                <h3>Физиотерапия</h3>
-                <Badge variant="default">10</Badge>
-              </div>
-              <Button variant="tretiary">Смотреть все</Button>
-            </div>
-
-            <div className="flex flex-wrap justify-between gap-[130px]">
-              <ImageCard
-                imageData={ImageCardMocData}
-                width={380}
-                height={418}
-              />
-              <ServiceCardsWrapper cardsList={CardsMocData} />
-            </div>
-          </div>
+          <CatigoryWrapper className="mb-[78px]" />
         </div>
       </section>
 
@@ -94,14 +56,10 @@ export default function Home() {
       </section>
 
       <section className="pt-[100px] pb-[100px]">
-      <Container>
-        <SliderWrapper ImageData={ImageMocData}/>
-      </Container>
-
+        <Container>
+          <SliderWrapper ImageData={ImageMocData} />
+        </Container>
       </section>
-        
-      
-
 
       {/* <AllButtons /> */}
     </div>
--- src/components/cards/serice-cards-wrapper.tsx
+++ src/components/cards/serice-cards-wrapper.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useClientMediaQuery } from "@/shared/hooks/useClientMediaQuery";
 import { ServiceCardItem } from "./service-card-item";
 
 export interface IServiceCards {
@@ -12,11 +15,16 @@ interface ServiceCardsWrapperProps {
 }
 
 const ServiceCardsWrapper: React.FC<ServiceCardsWrapperProps> = ({ cardsList }) => {
+  const isMobile = useClientMediaQuery('(max-width: 1280px)')
+  const numberOfCardsToRender = isMobile ? 2 : 4;
+
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:grid-rows-2 gap-[20px]">
-      {cardsList.map((item) => (
-        <ServiceCardItem key={item.id} card={item} />
-      ))}
+    <div className="grid grid-cols-1 xl:grid-cols-2 md:grid-rows-2 gap-[20px] justify-items-center">      
+        {
+        cardsList.slice(0, numberOfCardsToRender).map((item) => (
+          <ServiceCardItem key={item.id} card={item} />
+        ))
+      }
     </div>
   );
 };
