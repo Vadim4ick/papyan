bb8bd8d71f227a0550c33ffbddc32128c07ac63f
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-03 14:35:30 +0300

    about page: second section responcive styles done

--- src/app/oklinike/page.tsx
+++ src/app/oklinike/page.tsx
@@ -2,8 +2,14 @@ import { BadgeWithIcon } from "@/components/badge-with-icon";
 import { ContactCardV2 } from "@/components/cards/contact-card-v2";
 import { SectionHeader } from "@/components/section-header";
 import { SliderWrapper } from "@/components/slider/slider-wrapper";
+import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
-import { ContactData, ImageMocData } from "@/shared/const/moc-data";
+import {
+  ContactData,
+  expirience,
+  graduation,
+  ImageMocData,
+} from "@/shared/const/moc-data";
 import Image from "next/image";
 
 export default function About() {
@@ -75,7 +81,7 @@ export default function About() {
             title="Специалисты клиники"
             description="Наши специалисты используют передовые методы и индивидуальный подход, чтобы вы вновь почувствовали уверенность в своих движениях"
           />
-          <div>
+          <div className="flex flex-col-reverse items-center gap-y-[32px] md:items-start md:flex-row gap-x-[40px] mb-[32px] xl:mb-[52px] xl:gap-x-[48px]">
             <div className="items-center relative ] w-[350px] h-[467px] md:w-[357px] md:h-[482px] xl:w-[400px] xl:h-[540px] overflow-hidden rounded-md group">
               <Image
                 width={380}
@@ -84,16 +90,99 @@ export default function About() {
                 alt={"altText"}
                 className="h-full w-full object-cover"
               />
-            <div className="absolute bottom-0 w-full flex flex-col gap-[8px] p-[8px] z-20">
-              <Button className="w-full h-[48px]" variant="default">Записаться на прием</Button>
-              <Button className="w-full h-[48px] bg-[#EBEFF3]" variant="white">Подробнее</Button>
+              <div className="absolute bottom-0 w-full flex flex-col gap-[8px] p-[8px] z-20">
+                <Button className="w-full h-[48px]" variant="default">
+                  Записаться на прием
+                </Button>
+                <Button
+                  className="w-full h-[48px] bg-[#EBEFF3]"
+                  variant="white"
+                >
+                  Подробнее
+                </Button>
+              </div>
             </div>
+            <div className="flex flex-col gap-y-8 justify-between">
+              <div className="max-w-[377px] ">
+                <Badge variant="secondary" className="mb-[20px]">
+                  Заведущий клиникой
+                </Badge>
+                <h3 className="mb-[12px]">Папян Артем Месробович</h3>
+                <p className="text-[16px] leading-[22px] xl:text-[16px] xl:leading-[22px]">
+                  Является специалистом по ЛФК, а также работает в сфере
+                  ортопедии и травматологии. В качестве тренера по лечебной
+                  физкультуре он ведет работу с пациентами самых разных групп
+                </p>
+              </div>
+
+              <div className="w-full hidden xl:block">
+                <div className="mb-[32px]">
+                  <p className="leading-[21.86px] font-semibold mb-[8px]">
+                    Образование:
+                  </p>
+                  <ul className="text-[16px] leading-[22px]">
+                    {graduation.map((item, index) => (
+                      <li
+                        key={index}
+                        className="mb-[6px] last:mb-0 relative pl-[4px] before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[2px] before:bg-primary"
+                      >
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <p className="leading-[21.86px] font-semibold mb-[8px]">
+                    Опыт и достижения:
+                  </p>
+                  <ul className="text-[16px] leading-[22px]">
+                    {expirience.map((item, index) => (
+                      <li
+                        key={index}
+                        className="mb-[6px] last:mb-0 relative pl-[4px] before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[2px] before:bg-[#FF8A00]"
+                      >
+                        {item}
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
             </div>
-            <div></div>
+          </div>
+          <div className="w-full xl:hidden">
+            <div className="mb-[32px]">
+              <p className="leading-[21.86px] font-semibold mb-[8px]">
+                Образование:
+              </p>
+              <ul className="text-[16px] leading-[22px]">
+                {graduation.map((item, index) => (
+                  <li
+                    key={index}
+                    className="mb-[6px] last:mb-0 relative pl-[4px] before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[2px] before:bg-primary"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div>
+              <p className="leading-[21.86px] font-semibold mb-[8px]">
+                Опыт и достижения:
+              </p>
+              <ul className="text-[16px] leading-[22px]">
+                {expirience.map((item, index) => (
+                  <li
+                    key={index}
+                    className="mb-[6px] last:mb-0 relative pl-[4px] before:content-[''] before:absolute before:left-0 before:top-0 before:bottom-0 before:w-[2px] before:bg-[#FF8A00]"
+                  >
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         </div>
       </section>
     </>
   );
 }
-
--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,14 +4,14 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/shared/lib/utils"
  
 const badgeVariants = cva(
-  "inline-flex items-center rounded-[100px] border px-[10px] py-[2px] text-[12px] font-semibold min-w-[29px] ",
+  "inline-flex items-center rounded-[100px] border px-[10px] py-[2px] text-[12px] font-semibold min-w-[29px] tracking-tight",
   {
     variants: {
       variant: {
         default:
-          "bg-[#FFFFFF]/70 backdrop-blur-[20px] text-[#353535] text-[12px] leading-[15.6px] tracking-tight",
+          "bg-[#FFFFFF]/70 backdrop-blur-[20px] text-[#353535] text-[12px] leading-[15.6px]",
         secondary:
-          "border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80",
+          "border-transparent bg-[#F0F3F8] text-[#1467E1] text-[14px] leading-[18px] hover:bg-secondary/80",
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
