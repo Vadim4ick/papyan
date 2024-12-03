35f17ae157521f9feea1e1ebc23ff734e2e87f3e
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-03 21:56:51 +0300

    About page: doctors list with adaptive

--- src/shared/const/moc-data.ts
+++ src/shared/const/moc-data.ts
@@ -1,36 +1,40 @@
 import { TCategory, TService } from "../types/types";
 
-export const categories:TCategory[] = [
-  { 
+export const categories: TCategory[] = [
+  {
     id: "fizioterapiya",
     name: "Физиотерапия",
-    imageUrl:"https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
   },
-  { 
+  {
     id: "massazhi",
     name: "Массажи",
-    imageUrl:"https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
   },
-  { 
+  {
     id: "procedury",
     name: "Процедуры",
-    imageUrl:"https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
   },
-  { 
+  {
     id: "lfk",
     name: "ЛФК",
-    imageUrl:"https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
-  }
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+  },
 ];
 
-export const servicesList:TService[] = [
+export const servicesList: TService[] = [
   {
     id: "1",
     title: "PRP-терапия 1",
     description:
       "1 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "1000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
   {
     id: "2",
@@ -38,7 +42,7 @@ export const servicesList:TService[] = [
     description:
       "2 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "2000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
   {
     id: "3",
@@ -46,7 +50,7 @@ export const servicesList:TService[] = [
     description:
       "3 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "3000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
   {
     id: "4",
@@ -54,7 +58,7 @@ export const servicesList:TService[] = [
     description:
       "4 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "4000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
   {
     id: "5",
@@ -62,7 +66,7 @@ export const servicesList:TService[] = [
     description:
       "5 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "5000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
   {
     id: "6",
@@ -70,20 +74,20 @@ export const servicesList:TService[] = [
     description:
       "6 PRP-терапия — это инновационная процедура, основанная на введении плазмы пациента, обогащенной тромбоцитами плазмы",
     price: "6000",
-    categoryId:"fizioterapiya"
+    categoryId: "fizioterapiya",
   },
 ];
 
 export const ServicesTitlesList = servicesList.map((item: TService) => ({
-  title: item.title
-}) )
+  title: item.title,
+}));
 
 export const ImageCardMocData = {
   id: 1,
   imageUrl:
     "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
   altText: "string",
-  services: ServicesTitlesList
+  services: ServicesTitlesList,
 };
 
 export const ImageMocData = [
@@ -91,7 +95,7 @@ export const ImageMocData = [
     id: 1,
     imageUrl:
       "https://images.unsplash.com/photo-1630631729332-d564de91400f?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      
+
     altText: "string",
   },
   {
@@ -147,26 +151,65 @@ export const ImageMocData = [
 export const MainBunnerImageUrl =
   "https://images.unsplash.com/photo-1593012370132-c4390ff79e92?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-  export const SecondBunnerImageUrl =
+export const SecondBunnerImageUrl =
   "https://images.unsplash.com/photo-1593012370132-c4390ff79e92?q=80&w=2062&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
 
-
-
 export const ContactData = {
-    address: "Краснодар, Постовая улица, 23",
-    schedule: "Пн-Сб: 08:00—20:00",
-    phone: "+7 (918) 232-13-13",
-  };  
-
+  address: "Краснодар, Постовая улица, 23",
+  schedule: "Пн-Сб: 08:00—20:00",
+  phone: "+7 (918) 232-13-13",
+};
 
 export const graduation = [
   "Кубанский государственный медицинский университет (лечебное дело) (2014 г.)",
   "Кубанский государственный медицинский университет (анестезиология и реаниматология) (2015 г.)",
   "Курсы повышения квалификации",
-  "Кубанский государственный медицинский университет (травматология и ортопедия) (2016 г.)"
-]
+  "Кубанский государственный медицинский университет (травматология и ортопедия) (2016 г.)",
+];
 
 export const expirience = [
   "Общая практика",
-  "Врач травматолог-ортопед. МБУЗ «Детская городская поликлиника №3» (2016-2020 гг.)."
-]
+  "Врач травматолог-ортопед. МБУЗ «Детская городская поликлиника №3» (2016-2020 гг.).",
+];
+
+export const cpecialistsList = [
+  {
+    id: 1,
+    name: "Лиясов Андрей Игоревич",
+    cpeciality: ["Ортопед", "Травматолог"],
+    description:
+      "Является специалистом по ЛФК, а также работает в сфере ортопедии и травматологии. В качестве тренера по лечебной физкультуре он ведет работу с пациентами самых разных групп",
+    imageUrl:
+      "https://images.unsplash.com/photo-1630631729332-d564de91400f?q=80&w=2187&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    name: "Лиясов Андрей Игоревич",
+    cpeciality: ["Ортопед", "Травматолог"],
+    description:
+      "Является специалистом по ЛФК, а также работает в сфере ортопедии и травматологии. В качестве тренера по лечебной физкультуре он ведет работу с пациентами самых разных групп",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    altText: "string",
+  },
+  {
+    id: 3,
+    name: "Лиясов Андрей Игоревич",
+    cpeciality: ["Ортопед", "Травматолог"],
+    description:
+      "Является специалистом по ЛФК, а также работает в сфере ортопедии и травматологии. В качестве тренера по лечебной физкультуре он ведет работу с пациентами самых разных групп",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    altText: "string",
+  },
+  {
+    id: 4,
+    name: "Лиясов Андрей Игоревич",
+    cpeciality: ["Ортопед", "Травматолог"],
+    description:
+      "Является специалистом по ЛФК, а также работает в сфере ортопедии и травматологии. В качестве тренера по лечебной физкультуре он ведет работу с пациентами самых разных групп",
+    imageUrl:
+      "https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80",
+    altText: "string",
+  },
+];
