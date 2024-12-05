92c861fdcb514de3ea9c6f4341a8e34c6fb8309c
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-05 18:34:53 +0300

    alkii started

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority"
 import { cn } from "@/shared/lib/utils"
  
 const badgeVariants = cva(
-  "inline-flex items-center rounded-[100px] border px-[10px] py-[2px] text-[12px] font-semibold min-w-[29px] tracking-tight",
+  "inline-flex items-center justify-center rounded-[100px] border px-[10px] py-[2px] text-[12px] font-semibold min-w-[29px] tracking-tight",
   {
     variants: {
       variant: {
@@ -12,8 +12,8 @@ const badgeVariants = cva(
           "bg-[#FFFFFF]/70 backdrop-blur-[20px] text-[#353535] text-[12px] leading-[15.6px]",
         secondary:
           "border-transparent bg-[#F0F3F8] text-[#1467E1] text-[14px] leading-[18px] hover:bg-secondary/80",
-        destructive:
-          "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
+        discont:
+          "border-transparent bg-[#72D459] text-destructive-foreground w-[70px] h-[37px] text-center",
         outline: "text-foreground",
       },
     },
