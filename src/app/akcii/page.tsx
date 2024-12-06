e8c38e5084404176102e4ad7810029fee0ce656e
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-06 16:40:13 +0300

    discount galery grid

--- src/app/akcii/page.tsx
+++ src/app/akcii/page.tsx
@@ -1,43 +1,93 @@
-import { Badge } from "@/components/ui/badge";
-import { RussianRuble } from "lucide-react";
-import Image from "next/image";
+"use client";
 
-const regularPrice = 5000;
-const discount = 15;
-const newPrice = regularPrice - (regularPrice * discount) / 100;
+import { DiscountCard } from "@/components/cards/discount-card";
+import { useClientMediaQuery } from "@/shared/hooks/useClientMediaQuery";
+
+const data = [
+  { title: "Хивамат-терапия", newPrice: 100, regularPrice: 120, discount: 20 },
+  {
+    title: "Комплексное лечение заболеваний опорно-двигательного аппарата",
+    regularPrice: 240,
+    discount: 40,
+  },
+  {
+    title: "Лечебно-оздоровительный Массаж",
+    regularPrice: 360,
+    discount: 60,
+  },
+  {
+    title: "Мануальная терапия",
+    regularPrice: 120,
+    discount: 20,
+  },
+  {
+    title: "Мануальная терапия",
+    regularPrice: 240,
+    discount: 40,
+  },
+  { title: "Хивамат-терапия", regularPrice: 360, discount: 60 },
+  {
+    title: "Магнитотерапия высокой интенсивности",
+    regularPrice: 120,
+    discount: 20,
+  },
+  { title: "Хивамат-терапия", regularPrice: 360, discount: 60 },
+  { title: "Хивамат-терапия", regularPrice: 360, discount: 60 },
+  { title: "Хивамат-терапия", regularPrice: 360, discount: 60 },
+  { title: "Хивамат-терапия", regularPrice: 360, discount: 60 },
+];
 
 export default function Akcii() {
+  const isTablet = useClientMediaQuery("(max-width: 1024px)");
+  let rows = [data.slice(0, 2), data.slice(2, 4), data.slice(4)];
+
+  if (isTablet) {
+    rows = [
+      data.slice(0, 1),
+      data.slice(1, 3),
+      data.slice(3, 4),
+      data.slice(4),
+    ];
+  }
+
   return (
     <section className="pt-[32px] py-5 lg:pt-[64px] lg:pb-[100px]">
       <div className="container mx-auto max-w-[1364px] px-[20px]">
-        <div className="items-center relative ] w-[350px] h-[467px] md:w-[357px] md:h-[482px] xl:w-[400px] xl:h-[540px] overflow-hidden rounded-md group">
-          <Image
-            width={380}
-            height={418}
-            src="https://images.unsplash.com/photo-1515377905703-c4788e51af15?q=80&w=1770&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt={"altText"}
-            className="h-full w-full object-cover"
-          />
-          <div className="absolute bottom-5 w-full   ">
-            <div className="w-[302px] h-[49px] bg-[#FFFFFF] mx-auto rounded-[41px] flex justify-between items-center pl-[16px] pr-[6px]">
-              <div className="flex gap-[6px] items-center">
-                <p className=" relative text-[#353535] text-[20px] leading-[25px] tracking-tight font-semibold ">
-                  {newPrice}
-                  <span className="absolute top-[5px]">
-                    <RussianRuble size={17} strokeWidth={2.25} />
-                  </span>{" "}
-                </p>
-
-                <p className="ms-[18px] relative text-[#7B7B7B] text-[14px] line-through leading-[18px] tracking-tight  ">
-                  {regularPrice}
-                  <span className="absolute top-[2px]">
-                    <RussianRuble size={13} strokeWidth={2.25} />
-                  </span>{" "}
-                </p>
-              </div>
-              <Badge variant="discont">-15%</Badge>
+        <h1>{isTablet}</h1>
+        <div className="flex flex-wrap gap-4 ">
+          {rows.map((row, rowIndex) => (
+            <div
+              key={rowIndex}
+              className={`flex flex-wrap gap-4 w-full justify-between ${
+                rowIndex % 2 === 1 ? "flex-row-reverse" : ""
+              } flex gap-4`}
+            >
+              {row.map((card, index) => (
+                <DiscountCard
+                  key={index}
+                  card={card}
+                  className={`text-center text-xl font-extrabold 
+                  ${
+                    isTablet
+                      ? rowIndex === 0
+                        ? "w-[100%] bg-red-500"
+                        : rowIndex === 1
+                        ? "w-full md:w-[48%] bg-green-500"
+                        : rowIndex === 2
+                        ? "w-[100%] bg-yellow-500"
+                        : "w-full md:w-[48%] bg-purple-500"
+                      : rowIndex === 2
+                      ? "w-[32%]"
+                      : index === 0
+                      ? "w-[66%]"
+                      : "w-[32%]"
+                  }
+                `}
+                />
+                  
+              ))}
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
