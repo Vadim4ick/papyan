336c4c6bf92279e6e5698ada462024c8f525ea83
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-07 23:40:40 +0300

    type files count

--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -5,6 +5,34 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const pathImage = (img: string) => {
+export function pathImage(img: string) {
   return `${process.env.NEXT_PUBLIC_SERVER_URL}/assets/${img}`;
+}
+
+type FileType = {
+  directus_files_id: {
+    id: string;
+    title: string;
+    width: number | null;
+    type: string;
+    height: number | null;
+  };
 };
+
+export function countFileTypes<T extends FileType>(
+  files: T[]
+): { imageCount: number; videoCount: number } {
+  let imageCount = 0;
+  let videoCount = 0;
+
+  for (const file of files) {
+    const type = file.directus_files_id.type;
+    if (type.startsWith("image")) {
+      imageCount++;
+    } else if (type.startsWith("video")) {
+      videoCount++;
+    }
+  }
+
+  return { imageCount, videoCount };
+}
