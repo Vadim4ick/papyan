234984a970ac8ad582f1b4ce02ef1dc8b88b0e3c
Vadim <a-yva-2013@mail.ru>
2024-12-07 15:35:46 +0400

    реализация слайдера

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { ImageMocData } from "@/shared/const/moc-data";
 import { SliderWrapper } from "@/components/slider/slider-wrapper";
 import { Banner } from "@/components/banner";
 import { useGetHomePage } from "@/shared/hooks/services/pages/useGetHomePage";
 import { ServicesClinic } from "@/components/services-clinic";
+import { pathImage } from "@/shared/lib/utils";
+import Image from "next/image";
 
 export default function Home() {
   const { data } = useGetHomePage();
@@ -29,17 +30,44 @@ export default function Home() {
         />
       )}
 
-      <section className="pt-[100px] pb-[100px]">
-        <div className="container h-[494px] md:h-[520px] lg:h-[470px]  mx-auto max-w-[1364px]">
-          <SliderWrapper>
-            {ImageMocData.map((item) => (
-              <img key={item.id} src={item.imageUrl} alt={item.altText} />
-            ))}
-          </SliderWrapper>
-        </div>
-      </section>
+      {data && data?.home_page.sliderClinik.length > 0 && (
+        <section className="pt-[100px] pb-[100px]">
+          <div className="container md:h-[520px] lg:h-[470px]  mx-auto max-w-[1364px]">
+            <SliderWrapper>
+              {data?.home_page.sliderClinik.map(({ directus_files_id }) => {
+                const isVideo = directus_files_id.type?.startsWith("video/");
 
-      {/* <AllButtons /> */}
+                return isVideo ? (
+                  <video
+                    key={directus_files_id.id}
+                    autoPlay
+                    muted
+                    playsInline
+                    loop
+                    width={directus_files_id.width || 289}
+                    height={directus_files_id.height || 434}
+                    className="!h-[494px] max-md:!h-[434px]"
+                  >
+                    <source
+                      src={pathImage(directus_files_id.id)}
+                      type={directus_files_id.type}
+                    />
+                  </video>
+                ) : (
+                  <Image
+                    key={directus_files_id.id}
+                    width={directus_files_id.width || 289}
+                    height={directus_files_id.height || 434}
+                    src={pathImage(directus_files_id.id)}
+                    alt={directus_files_id.title}
+                    className="!h-[494px] max-md:!h-[434px]"
+                  />
+                );
+              })}
+            </SliderWrapper>
+          </div>
+        </section>
+      )}
     </div>
   );
 }
