958fb02c50e534e945becdccee0d5812dacb4cb3
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-07 22:33:49 +0300

    main page slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,13 @@ import { useGetHomePage } from "@/shared/hooks/services/pages/useGetHomePage";
 import { ServicesClinic } from "@/components/services-clinic";
 import { pathImage } from "@/shared/lib/utils";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { BadgeWithIcon } from "@/components/badge-with-icon";
 
 export default function Home() {
+  const router = useRouter();
   const { data } = useGetHomePage();
 
   return (
@@ -32,39 +37,91 @@ export default function Home() {
 
       {data && data?.home_page.sliderClinik.length > 0 && (
         <section className="pt-[100px] pb-[100px]">
-          <div className="container md:h-[520px] lg:h-[470px]  mx-auto max-w-[1364px]">
-            <SliderWrapper>
-              {data?.home_page.sliderClinik.map(({ directus_files_id }) => {
-                const isVideo = directus_files_id.type?.startsWith("video/");
+          <div className="container mx-auto max-w-[1364px] px-[20px]">
+            <div className="flex flex-col md:flex-row gap-[12px] justify-between mb-[28px] md:mb-[84px] lg:mb-[118px] ">
+              <div>
+                <h2 className="mb-[28px] md:mb-[32px] md:text-[36px] md:leading-[45px]">
+                  Клиника Доктора Папяна
+                </h2>
+                <Button
+                  variant="secondary"
+                  onClick={() => router.push(`/oklinike/`)}
+                  className="hidden md:flex"
+                >
+                  О клинике{" "}
+                  <span className="ms-[5px] mt-[3px]">
+                    <ChevronRight width={17} />
+                  </span>
+                </Button>
+              </div>
 
-                return isVideo ? (
-                  <video
-                    key={directus_files_id.id}
-                    autoPlay
-                    muted
-                    playsInline
-                    loop
-                    width={directus_files_id.width || 289}
-                    height={directus_files_id.height || 434}
-                    className="!h-[494px] max-md:!h-[434px]"
-                  >
-                    <source
+              <p className="text-[#595959] text-[16px] leading-[22px] md:max-w-[377px]">
+                Цель текста на сайте — привлечь пациента из поисковых систем и
+                убедить его в том, что ему нужна именно эта услуга и именно в
+                вашей клинике. Хорошая статья показывает компетентность врачей,
+                надежность клиники, и даже ее уровень оснащенности. И все это не
+                через голословные утверждения, а через заботу и пользу для
+                пациента.
+              </p>
+            </div>
+            <Button
+              variant="secondary"
+              onClick={() => router.push(`/oklinike/`)}
+              className="mb-[72px] md:hidden"
+            >
+              О клинике{" "}
+              <span className="ms-[5px] mt-[3px]">
+                <ChevronRight width={17} />
+              </span>
+            </Button>
+            <div className="md:h-[520px] lg:h-[470px]">
+              <div className="flex gap-[6px] mb-[24px] md:mb-[38px] xl:mb-[24px]">
+                <BadgeWithIcon
+                  className="bg-[#EBEFF3]"
+                  variant="video"
+                  tittle={"Видео"}
+                  quantity={10}
+                />
+                <BadgeWithIcon
+                  className="bg-[#EBEFF3]"
+                  variant="photo"
+                  tittle={"Фото"}
+                  quantity={10}
+                />
+              </div>
+              <SliderWrapper  className="h-[434px] md:h-[467px] xl:h-[474px]">
+                {data?.home_page.sliderClinik.map(({ directus_files_id }) => {
+                  const isVideo = directus_files_id.type?.startsWith("video/");
+
+                  return isVideo ? (
+                    <video
+                      key={directus_files_id.id}
+                      autoPlay
+                      muted
+                      playsInline
+                      loop
+                      width={directus_files_id.width || 289}
+                      height={directus_files_id.height || 434}
+                      className="h-full w-full object-cover"
+                    >
+                      <source
+                        src={pathImage(directus_files_id.id)}
+                        type={directus_files_id.type}
+                      />
+                    </video>
+                  ) : (
+                    <Image
+                      key={directus_files_id.id}
+                      width={directus_files_id.width || 289}
+                      height={directus_files_id.height || 434}
                       src={pathImage(directus_files_id.id)}
-                      type={directus_files_id.type}
+                      alt={directus_files_id.title}
+                      className="h-full w-full object-cover"
                     />
-                  </video>
-                ) : (
-                  <Image
-                    key={directus_files_id.id}
-                    width={directus_files_id.width || 289}
-                    height={directus_files_id.height || 434}
-                    src={pathImage(directus_files_id.id)}
-                    alt={directus_files_id.title}
-                    className="!h-[494px] max-md:!h-[434px]"
-                  />
-                );
-              })}
-            </SliderWrapper>
+                  );
+                })}
+              </SliderWrapper>
+            </div>
           </div>
         </section>
       )}
