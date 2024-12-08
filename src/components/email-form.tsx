704336acac75f53e84c1dcb9a0a3262495c1232e
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-08 18:28:40 +0300

    форма

--- src/components/email-form.tsx
+++ src/components/email-form.tsx
@@ -7,34 +7,31 @@ import {
   FormControl,
   FormField,
   FormItem,
-  FormLabel,
-  FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 
-const phoneValidation = new RegExp(
-  /^(?:\+7|8)?\s?\(?([1-9][0-9]{2})\)?\s?(\d{3})\s?(\d{2})\s?(\d{2})$/
-);
+const phoneValidation = /^(?:\+7|8)?\s?\(?[1-9]\d{2}\)?\s?\d{3}-?\d{2}-?\d{2}$/
 
-const formSchema = z.object({
-  name: z.string().min(1, {
-    message: "Укажите имя",
-  }),
-  email: z.string().email({
-    message: "Укажите почту",
-  }),
-  phone: z
-    .string()
-    .min(1, {
-      message: "Поле не может быть пустым",
-    })
-    .regex(phoneValidation, { message: "invalid phone" }),
-});
+const formSchema = z
+  .object({
+    name: z.string().min(1, {
+      message: "Укажите имя",
+    }),
+    email: z.string()
+    .email({message: "Укажите корректный email",})
+    .optional(),
+    phone: z.string()
+    .regex(phoneValidation, { message: "Введите корректный номер телефона" })
+      .optional(),
+  });
+
+
 
 export function EmailForm() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
+    mode: "onChange",
     defaultValues: {
       name: "",
       email: "",
@@ -47,6 +44,7 @@ export function EmailForm() {
   const onSubmit = (values: z.infer<typeof formSchema>) => {
     console.log("🚀 ~ onSubmit ~ values:", values);
   };
+  console.log("Errors:", form.formState.errors);
 
   return (
     <Form {...form}>
@@ -59,17 +57,21 @@ export function EmailForm() {
               <FormItem>
                 <FormControl>
                   <Input
-                  className="h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-[15px] leading-[20px] tracking-tight"
-                    placeholder="Ваше Имя"
+                    className={`h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-center text-[15px] leading-[20px] tracking-tight ${
+                      form.formState.errors.name ? "text-[#F52222] placeholder:text-[#F52222] border-[#F52222]" : ""
+                    }`}
+                    placeholder={
+                      form.formState.errors.name?.message || "Ваше Имя"
+                    }
                     disabled={isPending}
+
                     {...field}
                   />
                 </FormControl>
-                <FormMessage />
               </FormItem>
             )}
           />
-          <div className="flex gap-2 mb-[24px]">
+          <div className="flex flex-col xl:flex-row gap-2 mb-[24px]">
             <FormField
               control={form.control}
               name="email"
@@ -77,19 +79,22 @@ export function EmailForm() {
                 <FormItem>
                   <FormControl>
                     <Input
-                      className="w-[194px] h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-[15px] leading-[20px] tracking-tight"
-                      placeholder="E-mail"
-                      type="email"
+                      className={`w-full xl:w-[194px] h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-center text-[15px] leading-[20px] tracking-tight ${
+                        form.formState.errors.email ? "text-[#F52222] placeholder:text-[#F52222] border-[#F52222]" : ""
+                      }`}
+                      placeholder={
+                        form.formState.errors.email?.message || "E-mail"
+                      }
+                      // type="email"
                       autoComplete="email"
                       disabled={isPending}
                       {...field}
                     />
                   </FormControl>
-                  <FormMessage />
                 </FormItem>
               )}
             />
-            <div className="flex items-center justify-center w-fit">
+            <div className="flex w-full items-center justify-center xl:w-fit">
               <span className="">или</span>
             </div>
             <FormField
@@ -99,20 +104,27 @@ export function EmailForm() {
                 <FormItem>
                   <FormControl>
                     <Input
-                      className="w-[194px] h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-[15px] leading-[20px] tracking-tight"
-                      placeholder="Телефон"
-                      type="phone"
+                      className={`xl:w-[194px] h-[52px] bg-[#F0F3F8] border-[#E3E6EB] text-center text-[15px] leading-[20px] tracking-tight ${
+                        form.formState.errors.phone ? "text-[#F52222] placeholder:text-[#F52222] border-[#F52222]" : ""
+                      }`}
+                      placeholder={
+                        form.formState.errors.phone?.message || "Телефон"
+                      }
+                      type="tel"
                       disabled={isPending}
                       {...field}
                     />
                   </FormControl>
-                  <FormMessage />
                 </FormItem>
               )}
             />
           </div>
 
-          <Button type="submit" disabled={isPending} className="h-[52px] w-full">
+          <Button
+            type="submit"
+            disabled={isPending}
+            className="h-[52px] w-full"
+          >
             Продолжить
           </Button>
         </div>
