03fe094ee25f0b0bd6da323c8fb1839a5e7c3ff5
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-19 16:04:37 +0300

    новый сладер - остались стили

--- src/components/cards/image-card-one.tsx
+++ src/components/cards/image-card-one.tsx
@@ -4,26 +4,24 @@ import { ArrowUpRight } from "lucide-react";
 import { cn, pathImage } from "@/shared/lib/utils";
 import { RoundButton } from "../ui/round-button";
 import { MediaFragmentFragment } from "@/graphql/__generated__";
-import { Badge } from "../ui/badge";
 import { useRouter } from "next/navigation";
 
 interface ImageCardOneProps {
   imageData: MediaFragmentFragment;
   className?: string;
-  bages?: string[];
-  catigoryId?:string
+  catigoryId?: string;
 }
 
 const ImageCardOne: React.FC<ImageCardOneProps> = ({
   imageData,
   className,
-  catigoryId
+  catigoryId,
 }) => {
-  const router = useRouter()
+  const router = useRouter();
   return (
     <div
       className={cn(
-        "w-[350px] h-[262px] md:w-[357px] md:h-[394px] xl:w-[380px] xl:h-[418px]  relative space-y-3 overflow-hidden rounded-md group",
+        "relative space-y-3 overflow-hidden rounded-md group",
         className
       )}
     >
@@ -35,14 +33,8 @@ const ImageCardOne: React.FC<ImageCardOneProps> = ({
         className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
       />
 
-      <div className="absolute top-0 right-[14px]">
-        <RoundButton
-          variant="default"
-          onClick={() => router.push(`/uslugi/${catigoryId}`)}
-          className="opacity-0  group-hover:opacity-100 group-hover:translate-y-[14px] transition-all duration-300"
-        >
-          <ArrowUpRight />
-        </RoundButton>
+      <div className="absolute top-0 w-full">
+        <div className="mx-[14px] rounded-lg h-[42px] bg-white">{imageData.title}</div>
       </div>
     </div>
   );
