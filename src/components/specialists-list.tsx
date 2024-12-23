90688f006032b8f775f13c4153ade42c13413817
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-23 16:52:09 +0300

    о клинике правки внесены

--- src/components/specialists-list.tsx
+++ src/components/specialists-list.tsx
@@ -14,56 +14,59 @@ const SpecialistsList = ({
 }) => {
   const router = useRouter();
   return (
-      <div className="container mx-auto max-w-[1364px] px-[20px]">
-        <div className="flex flex-col xl:flex-row gap-y-[48px] gap-x-[20px] xl:flex-wrap ">
-          {doctors.map((doctor, index) => (
-            <div
-              key={doctor.id}
-              className={`md:flex ${
-                index % 2 === 1 ? "" : "md:flex-row-reverse"
-              } gap-x-[40px] xl:flex-col-reverse xl:gap-y-[12px]`}
-            >
-              <div className="md:w-1/2 xl:w-full">
-                <Separator className="mb-[32px] md:hidden" />
-                <Badge variant="secondary" className="mb-[16px] xl:mb-[8px]">
-                  {doctor.post}
-                </Badge>
-                <div className="w-full flex flex-col gap-y-[10px] mb-[10px]">
-                  {doctor?.name && (
-                    <h4 className="md:text-[28px] md:leading-[35px] xl:text-[16px] xl:leading-[22px]">
-                      {doctor.name}
-                    </h4>
-                  )}
+    <div className="container mx-auto max-w-[1364px] px-[20px]">
+      <div className="flex flex-col xl:flex-row gap-y-[48px] gap-x-[20px] xl:flex-wrap ">
+        {doctors.map((doctor, index) => (
+          <div
+            key={doctor.id}
+            className={`md:flex ${
+              index % 2 === 1 ? "" : "md:flex-row-reverse"
+            } gap-x-[40px] xl:flex-col-reverse xl:gap-y-[12px]`}
+          >
+            <div className="md:w-1/2 xl:w-full">
+              <Separator className="mb-[32px] md:hidden" />
+              <Badge
+                variant="secondary"
+                className="mb-[16px] xl:mb-[8px] py-[6px]"
+              >
+                {doctor.post}
+              </Badge>
+              <div className="w-full flex flex-col gap-y-[10px] mb-[10px]">
+                {doctor?.name && (
+                  <h4 className="md:text-[28px] md:leading-[35px] xl:text-[16px] xl:leading-[22px]">
+                    {doctor.name}
+                  </h4>
+                )}
 
-                  {doctor?.description && (
-                    <p className="md:text-[16px] md:leading-[22px] xl:hidden">
-                      {doctor.description}
-                    </p>
-                  )}
-                </div>
-              </div>
-              <div className="items-center relative ] w-[350px] h-[467px] md:w-[357px] md:h-[482px] xl:w-[316px] xl:h-[455px] overflow-hidden rounded-md group">
-                <Image
-                  width={380}
-                  height={418}
-                  src={pathImage(doctor.img.id)}
-                  alt={"altText"}
-                  className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
-                />
-                <div className="absolute top-0 right-[14px] z-20">
-                  <RoundButton
-                    className="opacity-0 group-hover:opacity-100 group-hover:translate-y-[14px] transition-all duration-300"
-                    variant="default"
-                    onClick={() => router.push(`/doctor/${doctor.id}`)}
-                  >
-                    <ArrowUpRight />
-                  </RoundButton>
-                </div>
+                {doctor?.description && (
+                  <p className="md:text-[16px] md:leading-[22px] xl:hidden">
+                    {doctor.description}
+                  </p>
+                )}
               </div>
             </div>
-          ))}
-        </div>
+            <div className="items-center relative ] w-[350px] h-[467px] md:w-[357px] md:h-[482px] xl:w-[316px] xl:h-[383px] overflow-hidden rounded-md group">
+              <Image
+                width={380}
+                height={418}
+                src={pathImage(doctor.img.id)}
+                alt={"altText"}
+                className="h-full w-full object-cover transition-transform duration-300 group-hover:scale-105"
+              />
+              <div className="absolute top-0 right-[14px] z-20">
+                <RoundButton
+                  className="opacity-0 group-hover:opacity-100 group-hover:translate-y-[14px] transition-all duration-300"
+                  variant="default"
+                  onClick={() => router.push(`/doctor/${doctor.id}`)}
+                >
+                  <ArrowUpRight />
+                </RoundButton>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
+    </div>
   );
 };
 
