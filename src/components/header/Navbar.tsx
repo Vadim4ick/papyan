490b0d2c3f7deb14fd181797456f1d40d6401887
RuslanKharisov <ruslan.kharisov@gmail.com>
2024-12-23 21:25:40 +0300

    fix bag

--- src/components/header/Navbar.tsx
+++ src/components/header/Navbar.tsx
@@ -6,23 +6,21 @@ import { Separator } from "../ui/separator";
 import { usePathname } from "next/navigation";
 
 interface NavbarProps {
-  onLinkClick?: () => void; // Функция, которая вызывается при клике на ссылку
+  onLinkClick?: () => void;
 }
 
 const Navbar: React.FC<NavbarProps> = ({ onLinkClick }) => {
   const pathname = usePathname()
-  console.log("🚀 ~ pathname:", pathname)
 
   return (
     <nav className="flex flex-col items-start text-sm font-medium lg:flex-row lg:h-10 py-2 bg-[#EDF1F5] rounded-lg">
       {navbar.map((item) => {
-        // Проверяем, является ли текущий элемент активным
         const isActive = pathname === item.path;
 
         return (
           <div key={item.id} className="h-full w-full lg:w-[132px] lg:flex lg:justify-between ">
             <Link
-              onClick={onLinkClick} // Закрываем меню при клике
+              onClick={onLinkClick}
               className={`flex items-center justify-center py-[11px] mx-auto lg:w-max
                 text-[24px] tracking-tight md:text-[32px] md:leading-[42px] lg:text-[14px] lg:leading-[19px] 
                 ${isActive ? 'text-[#0084FF]' : 'text-[#454545]'} // Активный элемент
