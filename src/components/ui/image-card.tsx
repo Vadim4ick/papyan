093885aa65d8a23bd8ae680a2f1a84dbe2c1f210
Vadim Firulyov <firulvv@mail.ru>
2025-01-02 17:45:25 +0500

    исправление большинства правок на гл странице

--- src/components/ui/image-card.tsx
+++ src/components/ui/image-card.tsx
@@ -6,6 +6,7 @@ import { MediaFragmentFragment } from "@/graphql/__generated__";
 import { Badge } from "./badge";
 import { useRouter } from "next/navigation";
 import { ArrowUpRight } from "@/shared/icons/ArrowUpRight";
+import Link from "next/link";
 
 interface ImageCardProps {
   imageData: MediaFragmentFragment;
@@ -22,46 +23,47 @@ const ImageCard: React.FC<ImageCardProps> = ({
 }) => {
   const router = useRouter();
   return (
-    <div
-      className={cn(
-        "w-[350px] h-[262px] md:w-[357px] md:h-[394px] xl:w-[380px] xl:h-[418px]  relative space-y-3 overflow-hidden rounded-md group",
-        className
-      )}
-    >
-      <Image
-        width={380}
-        height={418}
-        src={pathImage(imageData.id)}
-        alt={imageData.title}
-        className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
-      />
+    <Link href={`/uslugi/${catigoryId}`}>
+      <div
+        className={cn(
+          "w-[350px] h-[262px] md:w-[357px] md:h-[394px] xl:w-[380px] xl:h-[418px]  relative space-y-3 overflow-hidden rounded-md group",
+          className
+        )}
+      >
+        <Image
+          width={380}
+          height={418}
+          src={pathImage(imageData.id)}
+          alt={imageData.title}
+          className="h-full w-full object-cover transition-transform duration-500 group-hover:scale-105"
+        />
 
-      <div className="absolute -top-[24px] right-[14px]">
-        <RoundButton
-          variant="default"
-          onClick={() => router.push(`/uslugi/${catigoryId}`)}
-          className="opacity-0 group-hover:opacity-100 group-hover:translate-y-[24px] transition-all duration-700 h-9 w-9 ease-out"
-        >
-          <ArrowUpRight />
-        </RoundButton>
-      </div>
-
-      {bages && bages?.length > 0 && (
-        <div className="absolute -bottom-[10px] left-[14px] right-[14px] opacity-0 group-hover:opacity-100 group-hover:translate-y-[-24px] transition-all duration-700 ease-out">
-          <div className="flex flex-wrap justify-start gap-[5px] ">
-            {bages.map((service, idx) => (
-              <Badge
-                key={idx}
-                variant="default"
-                className="text-[12px] leading-[15.6px] px-2"
-              >
-                {service}
-              </Badge>
-            ))}
-          </div>
+        <div className="absolute -top-[24px] right-[14px]">
+          <RoundButton
+            variant="default"
+            onClick={() => router.push(`/uslugi/${catigoryId}`)}
+            className="opacity-0 group-hover:opacity-100 group-hover:translate-y-[24px] transition-all duration-700 h-9 w-9 ease-out"
+          >
+            <ArrowUpRight />
+          </RoundButton>
         </div>
-      )}
-    </div>
+        {bages && bages?.length > 0 && (
+          <div className="absolute -bottom-[10px] left-[14px] right-[14px] opacity-0 group-hover:opacity-100 group-hover:translate-y-[-24px] transition-all duration-700 ease-out">
+            <div className="flex flex-wrap justify-start gap-[5px] ">
+              {bages.map((service, idx) => (
+                <Badge
+                  key={idx}
+                  variant="default"
+                  className="text-[12px] leading-[15.6px] px-2"
+                >
+                  {service}
+                </Badge>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </Link>
   );
 };
 
