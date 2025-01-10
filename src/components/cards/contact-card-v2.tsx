ab59a42bf282c8993f28e33ee7b4ebb3aa2c05ac
RuslanKharisov <ruslan.kharisov@gmail.com>
2025-01-10 11:53:24 +0300

    формат телефона: обработка undefined

--- src/components/cards/contact-card-v2.tsx
+++ src/components/cards/contact-card-v2.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import { Card, CardContent, CardFooter } from "@/components/ui/card";
 import { cn, formattedPhoneNumber } from "@/shared/lib/utils";
-import InputMask from "react-input-mask";
 
 interface ContactCardProps {
   address: string | undefined;
@@ -18,9 +17,10 @@ function ContactCardV2({
   className,
   children,
 }: ContactCardProps) {
-  const formattedPhone = React.useMemo(()=> {
-      return formattedPhoneNumber (phone)
-    }, [phone])
+  const formattedPhone = React.useMemo(() => {
+    return phone ? formattedPhoneNumber(phone) : phone;
+  }, [phone]);
+
   return (
     <Card className={cn("w-full border-none py-7 px-7", className)}>
       <CardContent className="h-[164px] flex flex-col gap-[16px] rounded-lg p-0 mb-[31px] font-semibold  text-[18px] leading-[28px] tracking-tight">
