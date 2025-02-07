62f5c466e46cefe61d321c5f89b1d33e76afc61f
Vadim <a-yva-2013@mail.ru>
2025-02-08 00:00:04 +0400

    test

--- src/components/services-cost/services-costs-wrapper.tsx
+++ src/components/services-cost/services-costs-wrapper.tsx
@@ -15,7 +15,6 @@ const ServicesCostsWrapper = ({
         quantity={category.allServices.length}
         className="mb-[12px] md:mb-[20px] lg:mb-[24px]"
       />
-
       <div className="flex flex-col gap-y-[8px] mb-[60px] md:mb-[64px] lg:mb-[48px]">
         {category.allServices.map((item) => (
           <ServiceCostItem key={item.id} service={item} />
