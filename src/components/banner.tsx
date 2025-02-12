2a8da1a238ca1db8f7b6c1baaf74ff78e81d03bd
Vadim Firulyov <firulvv@mail.ru>
2025-02-12 13:13:18 +0400

    фикс качества банеров

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -1,6 +1,6 @@
 import { MediaFragmentFragment } from "@/graphql/__generated__";
 import { pathImage } from "@/shared/lib/utils";
-import Image from "next/image";
+// import Image from "next/image";
 
 const Banner = ({
   imageUrl,
@@ -10,23 +10,22 @@ const Banner = ({
   hight: string;
 }) => {
   return (
-    
-      <div className="lg:px-[20px]">
-        <div
-          className={`w-full overflow-hidden lg:container mx-auto lg:px-[20px] lg:max-w-[1364px] ${hight}`}
-        >
-          <Image
-            width={1324}
-            height={727}
-            src={pathImage(imageUrl.id)}
-            alt={"не удалось загрузить"}
-            className="h-full lg:rounded-lg
+    <div className="lg:px-[20px]">
+      <div
+        className={`w-full overflow-hidden lg:container mx-auto lg:px-[20px] lg:max-w-[1364px] ${hight}`}
+      >
+        <img
+          width={1324}
+          height={727}
+          src={pathImage(imageUrl.id)}
+          alt={"не удалось загрузить"}
+          className="h-full lg:rounded-lg
           w-full object-cover 
           transition-transform duration-300 group-hover:scale-105
           "
-          />
-        </div>
+        />
       </div>
+    </div>
   );
 };
 
