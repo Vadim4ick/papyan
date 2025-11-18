8ea155f01fa6b305c64a168d0cf7119f4cf34374
Vadim <a-yva-2013@mail.ru>
2025-11-18 21:13:29 +0400

    ✨ (layout.tsx): добавлен скрипт для обработки отправки форм и интеграции с Calltouch Добавлен скрипт, который отслеживает клики на кнопках отправки форм и собирает данные для отправки в Calltouch. Это улучшает функциональность приложения, позволяя собирать и отправлять информацию о заявках пользователей, что может помочь в анализе и повышении качества обслуживания клиентов.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,45 @@ export default function RootLayout({
         {/* /Yandex.Metrika */}
 
         <Toaster richColors />
+
+        <Script id="calltouch-requests" strategy="afterInteractive">
+          {`
+  Element.prototype.matches||(Element.prototype.matches=Element.prototype.matchesSelector||Element.prototype.webkitMatchesSelector||Element.prototype.mozMatchesSelector||Element.prototype.msMatchesSelector),Element.prototype.closest||(Element.pro>
+  var ct_get_val=function(form,selector){if(!!form.querySelector(selector)){return form.querySelector(selector).value;}else{return '';}}
+  
+  document.addEventListener('click', function(e) {
+      var t_el = e.target;
+      if (t_el.closest('form [type="submit"]')){ try {
+          var f = t_el.closest('form');
+          var fio = ct_get_val(f,'input[name="name"]');
+          var phone = ct_get_val(f,'input[name="phone"],input[placeholder*="+7(000)000-00-00"]');
+          var email = ct_get_val(f,'input[name="email"]');
+          var comment = ct_get_val(f,'textarea[placeholder="Дополнительная информация"]');
+          var sub = 'Заявка с ' + location.hostname;
+          var ct_data = {
+              fio: fio,
+              phoneNumber: phone,
+              email: email,
+              subject: sub,
+              requestUrl: location.href,
+              comment: comment,
+              sessionId: window.call_value
+          };
+          var post_data = Object.keys(ct_data).reduce(function(a,k){if(!!ct_data[k]){a.push(k+'='+encodeURIComponent(ct_data[k]));}return a},[]).join('&');
+          var site_id = '78669';
+          var CT_URL = 'https://api.calltouch.ru/calls-service/RestAPI/requests/'+site_id+'/register/';
+          var ct_valid = (!!phone || !!email) && !!fio;
+          console.log(ct_data,ct_valid);
+          if (ct_valid && !window.ct_snd_flag){
+              window.ct_snd_flag = 1; setTimeout(function(){ window.ct_snd_flag = 0; }, 20000);
+              var request = window.ActiveXObject?new ActiveXObject("Microsoft.XMLHTTP"):new XMLHttpRequest();
+              request.open("POST", CT_URL, true); request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+              request.send(post_data);
+          }
+      } catch (e) { console.log(e); } }
+  });
+`}
+        </Script>
       </body>
     </html>
   );
